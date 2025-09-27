7394cab214ceb4199ae1befd44c4c7fef40ec477
Ben4 <benymany511@gmail.com>
2025-09-27 23:06:57 +0300

    Initial commit: import/export

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,10 @@
 console.log('app worked');
+import Game, { GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame } from './game.js';
+
+const game = new Game();
+game.start();
+
+// заглушки для проверки: покажем, что мы действительно импортировали
+console.log(GameSavingData);
+console.log(loadGame);
+console.log(saveGame);
